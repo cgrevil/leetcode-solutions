06d2dbdff1dfb7dc74b8bd2684f890b9df3754ee
Christian Grevil <cg@enlight.dk>
2021-06-08 13:00:57 -0700

    Add red tests for strong-password-checker

---
+++ src/Solutions/strong-password-checker/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace Solutions.strong_password_checker
+{
+    /// <summary>
+    /// https://leetcode.com/problems/strong-password-checker/
+    /// </summary>
+    public class Solution
+    {
+        public int StrongPasswordChecker(string password)
+        {
+            return -1;
+        }
+    }
+}
---
+++ tests/Solutions.Tests/strong-password-checker/UnitTests.cs
@@ -0,0 +1,32 @@
+using Xunit;
+
+namespace Solutions.strong_password_checker
+{
+    public class UnitTests
+    {
+        private readonly Solution _solution;
+
+        public UnitTests()
+        {
+            _solution = new Solution();
+        }
+
+        [Fact]
+        public void Example1()
+        {
+            Assert.Equal(5, _solution.StrongPasswordChecker("a"));
+        }
+
+        [Fact]
+        public void Example2()
+        {
+            Assert.Equal(3, _solution.StrongPasswordChecker("aA1"));
+        }
+
+        [Fact]
+        public void Example3()
+        {
+            Assert.Equal(0, _solution.StrongPasswordChecker("1337C0d3"));
+        }
+    }
+}
