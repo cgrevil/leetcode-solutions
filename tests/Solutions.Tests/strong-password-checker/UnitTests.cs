ff1d4c8ab21379b8c7b6f49ffcef47eb24cf9755
Christian Grevil <cg@enlight.dk>
2021-06-23 12:22:52 -0700

    Additional tests

--- tests/Solutions.Tests/strong-password-checker/UnitTests.cs
+++ tests/Solutions.Tests/strong-password-checker/UnitTests.cs
@@ -28,5 +28,17 @@ namespace Solutions.strong_password_checker
         {
             Assert.Equal(0, _solution.StrongPasswordChecker("1337C0d3"));
         }
+
+        [Fact]
+        public void Example4()
+        {
+            Assert.Equal(2, _solution.StrongPasswordChecker("aaa111"));
+        }
+
+        [Fact]
+        public void Example5()
+        {
+            Assert.Equal(8, _solution.StrongPasswordChecker("bbaaaaaaaaaaaaaaacccccc"));
+        }
     }
 }
