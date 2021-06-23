2f67d82b454fc4d9eaf39961c2ce9ebfc8deeb96
Christian Grevil <cg@enlight.dk>
2021-06-23 13:12:38 -0700

    Implement print-foobar-alternately solution using semaphores

--- src/Solutions/print-foobar-alternately/FooBar.cs
+++ src/Solutions/print-foobar-alternately/FooBar.cs
@@ -1,24 +1,29 @@
 ﻿using System;
+using System.Threading;
 
 namespace Solutions.print_foobar_alternately
 {
     public class FooBar
     {
         private int n;
+        private SemaphoreSlim fooSemaphore;
+        private SemaphoreSlim barSemaphore;
 
         public FooBar(int n)
         {
             this.n = n;
+            this.fooSemaphore = new SemaphoreSlim(1);
+            this.barSemaphore = new SemaphoreSlim(0);
         }
 
         public void Foo(Action printFoo)
         {
-
             for (int i = 0; i < n; i++)
             {
-
+                fooSemaphore.Wait();
                 // printFoo() outputs "foo". Do not change or remove this line.
                 printFoo();
+                barSemaphore.Release();
             }
         }
 
@@ -27,9 +32,10 @@ namespace Solutions.print_foobar_alternately
 
             for (int i = 0; i < n; i++)
             {
-
+                barSemaphore.Wait();
                 // printBar() outputs "bar". Do not change or remove this line.
                 printBar();
+                fooSemaphore.Release();
             }
         }
     }
