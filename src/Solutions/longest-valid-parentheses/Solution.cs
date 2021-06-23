105d14ce45e411e9a8db7a9768f273602a30a45b
Christian Grevil <cg@enlight.dk>
2021-06-23 13:53:20 -0700

    Implement solution for longest_valid_parentheses

--- src/Solutions/longest-valid-parentheses/Solution.cs
+++ src/Solutions/longest-valid-parentheses/Solution.cs
@@ -1,10 +1,41 @@
-﻿namespace Solutions.longest_valid_parentheses
+﻿using System;
+
+namespace Solutions.longest_valid_parentheses
 {
     public class Solution
     {
         public int LongestValidParentheses(string s)
         {
-            return -1;
+            int longest = 0;
+
+            int currentLength = 0;
+            int currentDepth = 0;
+
+            foreach (char c in s)
+            {
+                if (c == '(')
+                {
+                    currentDepth++;
+                }
+                else if (c == ')')
+                {
+                    if (currentDepth == 0)
+                    {
+                        currentLength = 0;
+                        continue;
+                    }
+
+                    currentDepth--;
+                    currentLength += 2;
+                    longest = Math.Max(longest, currentLength);
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+            return longest;
         }
     }
 }
