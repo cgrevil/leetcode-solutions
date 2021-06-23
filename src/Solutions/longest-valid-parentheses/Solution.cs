7486e38dc13dbd1a1a5ec10bd7e9a2cf65eb6672
Christian Grevil <cg@enlight.dk>
2021-06-23 13:24:31 -0700

    Add solution template and example tests for longest-valid-parentheses

---
+++ src/Solutions/longest-valid-parentheses/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace Solutions.longest_valid_parentheses
+{
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            return -1;
+        }
+    }
+}
---
+++ tests/Solutions.Tests/longest-valid-parentheses/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Solutions.longest_valid_parentheses
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("(()", 2)]
+        [InlineData(")()())", 4)]
+        [InlineData("", 0)]
+        public void SolutionReturnsLengthOfLongestValidParenthesesString(string s, int expectedOutput)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            int output = solution.LongestValidParentheses(s);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
