9d36d98556f6d7e9fc1fc40cb34eec6bcfe54ef0
Christian Grevil <cg@enlight.dk>
2021-06-24 07:08:17 -0700

    Add solution template and red tests for median_of_two_sorted_arrays

---
+++ src/Solutions/median-of-two-sorted-arrays/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Solutions.median_of_two_sorted_arrays
+{
+    /// <summary>
+    /// https://leetcode.com/problems/median-of-two-sorted-arrays/
+    /// </summary>
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Solutions.Tests/median-of-two-sorted-arrays/UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Solutions.median_of_two_sorted_arrays
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("1,3", "2", 2.0)]
+        [InlineData("1,2", "3,4", 2.5)]
+        [InlineData("0,0", "0,0", 0.0)]
+        [InlineData("", "1", 1.0)]
+        [InlineData("2", "", 2.0)]
+        public void SolutionFindsTheMedianOfSortedArraysTheory(string nums1String, string nums2String, double expectedOutput)
+        {
+            // Arrange
+            var nums1 = ParseNumString(nums1String);
+            var nums2 = ParseNumString(nums2String);
+            
+            var solution = new Solution();
+
+            // Act
+            double output = solution.FindMedianSortedArrays(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+        private static int[] ParseNumString(string numsString)
+        {
+            return numsString
+                .Split(",")
+                .Where(s => s != "")
+                .Select(n => int.Parse(n))
+                .ToArray();
+        }
+    }
+}
