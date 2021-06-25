80a3ecf0af51c5ea143ce9038aa253f17c7fe7ef
Christian Grevil <cg@enlight.dk>
2021-06-25 13:26:27 -0700

    Add additional tests for regular_expression_matching

--- tests/Solutions.Tests/regular-expression-matching/UnitTests.cs
+++ tests/Solutions.Tests/regular-expression-matching/UnitTests.cs
@@ -10,6 +10,8 @@ namespace Solutions.regular_expression_matching
         [InlineData("aa", ".*", true)]
         [InlineData("aab", "c*a*b", true)]
         [InlineData("mississippi", "mis*is*p*.", false)]
+        [InlineData("mississippi", "mis*is*.p*.", true)]
+        [InlineData("a", "ab*", true)]
         public void RegularExpressionMatchingTheory(string input, string pattern, bool expectedOutput)
         {
             // Arrange
