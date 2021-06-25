083d8bd410696f5ad92c6d35ba15f208faaebc46
Christian Grevil <cg@enlight.dk>
2021-06-25 10:57:14 -0700

    Add solution template and red tests for regular_expression_matching

---
+++ src/Solutions/regular-expression-matching/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solutions.regular_expression_matching
+{
+    public class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/Solutions.Tests/regular-expression-matching/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Solutions.regular_expression_matching
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData("aa", "a", false)]
+        [InlineData("aa", "a*", true)]
+        [InlineData("aa", ".*", true)]
+        [InlineData("aab", "c*a*b", true)]
+        [InlineData("mississippi", "mis*is*p*.", false)]
+        public void RegularExpressionMatchingTheory(string input, string pattern, bool expectedOutput)
+        {
+            // Arrange
+            var solution = new Solution();
+
+            // Act
+            bool output = solution.IsMatch(input, pattern);
+
+            // Assert
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
